908d48dc575630c7dcdf12576304118a996dfc78
Newman <newmer8@ad.vzwcorp.com>
2021-11-22 15:02:06 -0600

    initial commit

---
+++ passenger.js
@@ -0,0 +1,9 @@
+const Person = require('./person')
+
+class Passenger extends Person {
+    constructor(name, bags, passportNumber, seatNumber){
+        super(name,bags)
+        this.passportNumber = passportNumber
+        this.seatNumber = seatNumber
+    }
+}
---
+++ person.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name, bags)
+        this.name = name
+        this.bags =bags
+        }
+        addBag(bag){
+            this.bags.push(bag)
+        }
+    }
+
+    module.exports = Person
